2ce3fe0595ac62fdde3f3277b04107aaf55b7694
iamnotpi <ductrung27.dt@gmail.com>
2024-08-21 16:49:19 +0700

    Add RoPE, update RMSNorm, fix attention bug and implement the model class.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Pi-Llama is a small language model that is capable of telling stories.
 ## TODOs
 - [ ] SwiGLU activation function
-- [x] RMS Normalization
 - [ ] RoPE
+- [ ] Hyperparameters (except for optimizer-related)
--- pillama.py
+++ pillama.py
@@ -1,3 +1,4 @@
+import math
 import torch 
 import torch.nn as nn
 from torch.nn import functional as F
@@ -10,12 +11,14 @@ with open("D:\\Code\\Python\\input.txt", 'r', encoding='utf-8') as file:
 @dataclass
 class PiLlamaConfig:
     n_embd: int = 768
-    block_size: int = 1024
+    block_size: int = 2048
     n_heads: int = 12
     kv_heads: int = 4
     n_layers: int = 4
     vocab_size: int = 50257
     flash_attention: bool = True
+    rope_base: int = 10000
+
 
 class RMSNorm(nn.Module):
     def __init__(self, n_embd, eps=1e-6):
@@ -25,8 +28,8 @@ class RMSNorm(nn.Module):
         self.eps = eps
 
     def forward(self, x):
-        norm = torch.linalg.norm(x, 2, dim=-1, keepdims=True)
-        return x / (norm + self.eps) * self.gain * self.n_embd ** 0.5
+        inv_norm = torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+        return x * inv_norm * self.gain 
         
 
 class GroupedQueryAttention(nn.Module):
@@ -41,6 +44,9 @@ class GroupedQueryAttention(nn.Module):
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
         self.register_buffer("mask", torch.tril(torch.ones((config.block_size, config.block_size))).view(1, 1, config.block_size, config.block_size))
 
+    def _rotate_half(self, x):
+        return torch.stack((-x[..., 1::2], x[..., ::2]), dim=-1).view(x.shape)
+    
     def forward(self, x):
         B, T, C = x.shape
         q = self.query(x).view(B, T, self.config.n_heads, self.head_size).transpose(1, 2) # (B, nh, T, head_size)
@@ -50,12 +56,22 @@ class GroupedQueryAttention(nn.Module):
         k = k.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=1) # (B, nh, T, head_size)
         v = v.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=1) # (B, nh, T, head_size)
 
+        # Rotary positional embedding
+        inv_freqs = 1.0 / self.config.rope_base ** (torch.arange(0, self.head_size, 2, device=x.device).float() / self.head_size)
+        theta = torch.outer(torch.arange(1, T + 1), inv_freqs)
+        pos = torch.repeat_interleave(theta, 2, dim=-1) 
+
+        rotated_q = self._rotate_half(q)
+        rotated_k = self._rotate_half(k)
+        q = q * torch.cos(pos) + rotated_q * torch.sin(pos)
+        k = k * torch.cos(pos) + rotated_k * torch.sin(pos)
+
         if self.config.flash_attention:
             y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
         else:
-            attn = (q @ k.transpose(-1, -2) * C ** -0.5)
-            attn = F.softmax(attn, dim=-1)
+            attn = (q @ k.transpose(-1, -2)) * (1.0 / math.sqrt(self.head_size))
             attn = attn.masked_fill(self.mask[:,:,:T,:T]==0, float('-inf'))
+            attn = F.softmax(attn, dim=-1)
             y = attn @ v
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         return self.c_proj(y)
@@ -75,7 +91,7 @@ class MLP(nn.Module):
 
 
 class Block(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config: PiLlamaConfig):
         super().__init__()
         self.attn = GroupedQueryAttention(config)
         self.rmsn_attn = RMSNorm(config.n_embd)
@@ -88,4 +104,29 @@ class Block(nn.Module):
         return x
 
 class PiLlama(nn.Module):
-    pass
+    def __init__(self, config: PiLlamaConfig):
+        super().__init__()
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layers)]),
+            rmsn = RMSNorm(config.n_embd)
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+        # Weights sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+    def forward(self, idx, targets=None):
+        B, T = idx.size()
+        x = self.transformer.wte(idx)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.rmsn(x)
+        logits = self.lm_head(x) # (B, T, n_embd)
+        loss = None
+        if targets is not None: 
+            loss = F.cross_entropy(logits.view(B * T, -1), targets.view(-1))
+        return logits, loss
+
+model = PiLlama(PiLlamaConfig())
+num_params = sum(p.numel() for p in model.parameters())
+print(f"Number of parameters: {num_params}")
