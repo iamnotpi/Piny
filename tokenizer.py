a4777562562ce5b06127bfb23a59139064c7c621
iamnotpi <ductrung27.dt@gmail.com>
2024-08-22 16:30:31 +0700

    Reorganize tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -2,33 +2,44 @@ import os
 from sentencepiece import SentencePieceProcessor, SentencePieceTrainer
 
 class Tokenizer:
-    def __init__(self, model_path):
-        assert os.path.isfile(model_path)
+    def __init__(self, model_path=None):
+        self.sp_model = None
+        if model_path:
+            self.load_model(model_path)
+
+    def load_model(self, model_path):
+        if not os.path.isfile(model_path):
+            raise FileNotFoundError(f"Model file not found: {model_path}")
+        
         self.sp_model = SentencePieceProcessor(model_file=model_path)
         self.vocab_size = self.sp_model.vocab_size()
-        self.bos_id: int = self.sp_model.bos_id()
-        self.eos_id: int = self.sp_model.eos_id()
+        self.bos_id = self.sp_model.bos_id()
+        self.eos_id = self.sp_model.eos_id()
 
-    def train(self, input, model_prefix='pi_tokenizer', vocab_size=16384):
+    def train(self, input_file, model_prefix='pi_tokenizer', vocab_size=16384):
         SentencePieceTrainer.train(
-            input = input, 
-            model_prefix = model_prefix, 
-            model_type = 'bpe', 
-            vocab_size = vocab_size, 
-            byte_fallback = True,
-            split_by_number = True
+            input=input_file, 
+            model_prefix=model_prefix, 
+            model_type='bpe', 
+            vocab_size=vocab_size, 
+            byte_fallback=True,
+            split_digits=True
         )
-        self.vocab_size = vocab_size
+        self.load_model(f"{model_prefix}.model")
 
-    def encode(self, text, bos, eos):
-        t = self.sp_model.encode_as_ids(text)
-        # Whether to prepend the beginning-of-sequence token
-        if bos: 
-            t = [self.bos_id] + t
-        # Whether to prepend the end-of-sequence token
+    def encode(self, text, bos=False, eos=False):
+        if not self.sp_model:
+            raise ValueError("Model is not loaded.")
+        
+        token_ids = self.sp_model.encode_as_ids(text)
+        if bos:
+            token_ids = [self.bos_id] + token_ids
         if eos:
-            t = t + [self.eos_id]
-        return t
+            token_ids = token_ids + [self.eos_id]
+        return token_ids
 
-    def decode(self, idx):
-        return self.sp_model.decode_ids(idx)
+    def decode(self, token_ids):
+        if not self.sp_model:
+            raise ValueError("Model is not loaded.")
+        
+        return self.sp_model.decode_ids(token_ids)
