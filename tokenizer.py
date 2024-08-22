fdfcd5bfca6a0c88c14a5ae15d166228cb4213d0
iamnotpi <ductrung27.dt@gmail.com>
2024-08-22 14:59:20 +0700

    Add `train()` method to Tokenizer class, remove `spmtrainer.py`

--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,24 @@
 import os
-from sentencepiece import SentencePieceProcessor
+from sentencepiece import SentencePieceProcessor, SentencePieceTrainer
 
 class Tokenizer:
     def __init__(self, model_path):
         assert os.path.isfile(model_path)
         self.sp_model = SentencePieceProcessor(model_file=model_path)
+        self.vocab_size = self.sp_model.vocab_size()
         self.bos_id: int = self.sp_model.bos_id()
         self.eos_id: int = self.sp_model.eos_id()
+
+    def train(self, input, model_prefix='pi_tokenizer', vocab_size=16384):
+        SentencePieceTrainer.train(
+            input = input, 
+            model_prefix = model_prefix, 
+            model_type = 'bpe', 
+            vocab_size = vocab_size, 
+            byte_fallback = True,
+            split_by_number = True
+        )
+        self.vocab_size = vocab_size
 
     def encode(self, text, bos, eos):
         t = self.sp_model.encode_as_ids(text)
