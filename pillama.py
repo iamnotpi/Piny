3c1c721dfd0234971be4c3680f97539e662a5c3a
iamnotpi <ductrung27.dt@gmail.com>
2024-08-26 23:10:40 +0700

    Add KV Cache!!!

--- pillama.py
+++ pillama.py
@@ -63,8 +63,6 @@ class PiLlamaConfig:
     max_lr: int = 9e-4
     max_batch_size: int = 262144
 
-B, T = 32, 2048
-
 class RMSNorm(nn.Module):
     def __init__(self, n_embd, eps=1e-6):
         super().__init__()
@@ -80,19 +78,36 @@ class RMSNorm(nn.Module):
 class KVCache(nn.Module):
     def __init__(self, batch_size, max_seq_length, n_kv_heads, head_size, device):
         super().__init__()
-        self.register_buffer("cache_k", torch.zeros((batch_size, max_seq_length, n_kv_heads, head_size), device=device))
-        self.register_buffer("cache_v", torch.zeros((batch_size, max_seq_length, n_kv_heads, head_size), device=device))
+        self.register_buffer("cache_k", torch.zeros((batch_size, n_kv_heads, max_seq_length, head_size), device=device))
+        self.register_buffer("cache_v", torch.zeros((batch_size, n_kv_heads, max_seq_length, head_size), device=device))
 
     def update(self, start_pos, k, v):
-        # k shape: (B, T, n_kv_heads, head_size)
-        seq_length = k.size(1)
+        # k shape: (B, n_kv_heads, T, head_size)
+        seq_length = k.size(2)
         # Add the new keys and values to the cache
-        self.cache_k[:, start_pos:start_pos + seq_length] = k
-        self.cache_v[:, start_pos:start_pos + seq_length] = v
+        self.cache_k[:, :, start_pos:start_pos + seq_length] = k
+        self.cache_v[:, :, start_pos:start_pos + seq_length] = v
         # Return the updated cache (all keys and values from the beginning up to the current position) 
-        k = self.cache_k[:, :start_pos + seq_length]    
-        v = self.cache_v[:, :start_pos + seq_length]
+        k = self.cache_k[:, :, :start_pos + seq_length]    
+        v = self.cache_v[:, :, :start_pos + seq_length]
         return k, v
+
+def rotate_half(x):
+    return torch.stack((-x[..., 1::2], x[..., ::2]), dim=-1).view(x.shape)
+    
+def inv_freqs(dim, seq_len, base=10000):
+    inv_freqs = 1.0 / base ** (torch.arange(0, dim, 2).float() / dim)
+    theta = torch.outer(torch.arange(1, seq_len + 1), inv_freqs)
+    pos = torch.repeat_interleave(theta, 2, dim=-1) 
+    return pos
+
+def apply_rope(x, inv_freqs, start_pos):
+    # x shape: (B, nh, T, head_size)
+    if start_pos == -1:
+        start_pos = 0
+    pos = inv_freqs[start_pos:start_pos + x.size(2)]
+    rotated_x = torch.stack((-x[..., 1::2], x[..., ::2]), dim=-1).view(x.shape)
+    return x * torch.cos(pos) + rotated_x * torch.sin(pos)
 
 
 class GroupedQueryAttention(nn.Module):
@@ -105,53 +120,44 @@ class GroupedQueryAttention(nn.Module):
         self.key = nn.Linear(config.n_embd, config.kv_heads * self.head_size, bias=False)
         self.value = nn.Linear(config.n_embd, config.kv_heads * self.head_size, bias=False)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # Mask is deprecated, preseved for compatibility with pretrained models
         self.register_buffer("mask", torch.tril(torch.ones((config.block_size, config.block_size))).view(1, 1, config.block_size, config.block_size))
-        # self.cache = None
-
-    def _rotate_half(self, x):
-        return torch.stack((-x[..., 1::2], x[..., ::2]), dim=-1).view(x.shape)
+        self.inv_freqs = inv_freqs(self.head_size, config.block_size, base=config.rope_base)
+        self.cache = None
     
-    def forward(self, x, mask=None):
-        B, T, C = x.shape
-        # q = self.query(x).view(B, T, self.config.n_heads, self.head_size) # (B, T, nh, head_size)
-        # k = self.key(x).view(B, T, self.config.kv_heads, self.head_size) # (B, T, nkvh, head_size)
-        # v = self.value(x).view(B, T, self.config.kv_heads, self.head_size) # (B, T, nkvh, head_size)
+    def forward(self, x, start_pos, mask=None):
+        # KV cache!!!
+        # Given |----cache_len----|----T----|; our model generates the logits for the next token
+        # start_pos = cache_len
+        B, T, C = x.shape # T is the length of the input sequence 
+
+        # q is the query of the current tokens, k and v are the keys and values from the cache and the current tokens
+        # q, k, v of the current tokens
         q = self.query(x).view(B, T, self.config.n_heads, self.head_size).transpose(1, 2) # (B, nh, T, head_size)
-        k = self.key(x).view(B, T, self.config.kv_heads, self.head_size).transpose(1, 2) # (B, nkvh, T, head_size)
-        v = self.value(x).view(B, T, self.config.kv_heads, self.head_size).transpose(1, 2) # (B, nkvh, T, head_size)
+        k = self.key(x).view(B, T, self.config.kv_heads, self.head_size).transpose(1, 2) # (B, nkvh, cache_len + T, head_size)
+        v = self.value(x).view(B, T, self.config.kv_heads, self.head_size).transpose(1, 2) # (B, nkvh, cache_len + T, head_size)
 
-        # if self.cache is not None:
-        #     k, v = self.cache.update(T, k, v)
+        # Update the keys and values with the cache (now containing the keys and values from all previous tokens)
+        if self.cache is not None:
+            k, v = self.cache.update(start_pos, k, v)
 
-        # k = k.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=2) # (B, cache_len + T, nh, head_size)
-        # v = v.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=2) # (B, cache_len + T, nh, head_size)
-        k = k.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=1) # (B, nh, T, head_size)
-        v = v.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=1) # (B, nh, T, head_size)
+        # Grouped query attention
+        k = k.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=1) # (B, nh, cache_len + T, head_size)
+        v = v.repeat_interleave(self.config.n_heads // self.config.kv_heads, dim=1) # (B, nh, cache_len + T, head_size)
 
-        # Rotary positional embedding
-        inv_freqs = 1.0 / self.config.rope_base ** (torch.arange(0, self.head_size, 2, device=x.device).float() / self.head_size)
-        theta = torch.outer(torch.arange(1, T + 1, device=x.device), inv_freqs)
-        pos = torch.repeat_interleave(theta, 2, dim=-1) 
-
-        # q = q.transpose(1, 2) # (B, nh, T, head_size)
-        # k = k.transpose(1, 2) # (B, nh, cache_len + T, head_size)
-        # v = v.transpose(1, 2) # (B, nh, cache_len + T, head_size)
-
-        rotated_q = self._rotate_half(q)
-        rotated_k = self._rotate_half(k)
-        q = q * torch.cos(pos) + rotated_q * torch.sin(pos)
-        k = k * torch.cos(pos) + rotated_k * torch.sin(pos)
+        q = apply_rope(q, self.inv_freqs, start_pos)
+        k = apply_rope(k, self.inv_freqs, start_pos)
 
         if self.config.flash_attention:
-            y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
-            # y = F.scaled_dot_product_attention(q, k, v, attn_mask=mask)
+            y = F.scaled_dot_product_attention(q, k, v, attn_mask=mask)
         else:
             attn = (q @ k.transpose(-1, -2)) * (1.0 / math.sqrt(self.head_size)) # (B, nh, T, cache_len + T)
-            # attn = attn + mask
-            attn = attn.masked_fill(self.mask[:,:,:T,:T]==0, float('-inf'))
+            if mask is not None:
+                attn = attn + mask
+            # attn = attn.masked_fill(self.mask[:,:,:T,:T]==0, float('-inf'))
             attn = F.softmax(attn, dim=-1)
             y = attn @ v # (B, nh, T, head_size)
-        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = y.transpose(1, 2).contiguous().view(x.shape)
         return self.c_proj(y)
 
 
@@ -175,8 +181,8 @@ class Block(nn.Module):
         self.mlp = MLP(config)
         self.rmsn_mlp = RMSNorm(config.n_embd)
     
-    def forward(self, x):
-        x = x + self.attn(self.rmsn_attn(x))
+    def forward(self, x, start_pos, mask=None):
+        x = x + self.attn(self.rmsn_attn(x), start_pos, mask)
         x = x + self.mlp(self.rmsn_mlp(x))
         return x
 
@@ -195,6 +201,7 @@ class PiLlama(nn.Module):
         self.transformer.wte.weight = self.lm_head.weight
         self.tokenizer = tokenizer
 
+    # Deprecated
     def forward(self, idx, targets=None):
         B, T = idx.size()
         x = self.transformer.wte(idx)
@@ -208,21 +215,30 @@ class PiLlama(nn.Module):
         return logits, loss if loss is not None else logits
     
     def forward_loss(self, idx, targets):
+        # For use during training
         B, T = idx.size()
+        mask = torch.full((T, T), float('-inf'), device=idx.device)
+        mask.triu_(diagonal=1)
         x = self.transformer.wte(idx)
         for block in self.transformer.h:
-            x = block(x)
+            x = block(x, -1, mask) # -1 disable the cache
         x = self.transformer.rmsn(x)
         logits = self.lm_head(x) # (B, T, n_embd)
         loss = F.cross_entropy(logits.view(B * T, -1), targets.view(-1))
-        return logits, loss 
+        return loss 
     
     @torch.inference_mode()
-    def forward_inference(self, idx):
-        B, T = idx.size()
+    def forward_inference(self, idx, start_pos):
+        # For use during inference
+        # start_pos: The starting position in the sequence of the generated tokens
+        T = idx.size(1)
+        mask = torch.full((T, T), float('-inf'), device=idx.device)
+        mask.triu_(diagonal=1)
+        # Since we are using KVCache, we need to pad the mask with zeros
+        mask = torch.cat([torch.zeros((T, start_pos), device=idx.device), mask], dim=-1) # (T, T + start_pos)
         x = self.transformer.wte(idx)
         for block in self.transformer.h:
-            x = block(x)
+            x = block(x, start_pos, mask)
         x = self.transformer.rmsn(x)
         logits = self.lm_head(x)
         return logits
@@ -245,41 +261,78 @@ class PiLlama(nn.Module):
     
     @torch.no_grad()
     def generate(self, tokens, max_gen_len, top_p, temperature, generator):
-        # tokens: A list of token ids
+        # tokens: List[List[int]]: List of tokenized texts
         # max_gen_len: Maximum length of the generated text
         self.eval()
+        device = self.lm_head.weight.device
+
+        # Setup KVCache for each block
+        min_prompt_len = min(len(t) for t in tokens)
+        max_prompt_len = max(len(t) for t in tokens)
+        assert max_prompt_len <= self.config.block_size
+        total_len = min(max_prompt_len + max_gen_len, self.config.block_size)
+        batch_size = len(tokens)
+
+        for block in self.transformer.h:
+            block.attn.cache = KVCache(
+                batch_size = batch_size,
+                max_seq_length = total_len,
+                n_kv_heads = self.config.kv_heads,
+                head_size = self.config.n_embd // self.config.n_heads,
+                device = device
+            )
+        
+        # Store all the given tokens in a tensor of size (batch_size, total_len)
+        tokens_tensor = torch.full((batch_size, total_len), self.tokenizer.pad_id, dtype=torch.long, device=device)
+        for i, token_ids in enumerate(tokens):
+            tokens_tensor[i, :len(token_ids)] = torch.tensor(token_ids, dtype=torch.long)
+        input_mask = tokens_tensor != self.tokenizer.pad_id # Mask the padding tokens
+        eos_reached = torch.zeros(batch_size, dtype=torch.bool, device=device)
+
+        prev_pos = 0 # Starting position of the generated tokens
+        for cur_pos in range(min_prompt_len, total_len): 
+            x = tokens_tensor[:, prev_pos:cur_pos] # Either (B, min_prompt_len) or (B, 1)
+            logits = self.forward_inference(x, prev_pos) # Either (B, min_prompt_len) or (B, 1)
+            logits = logits[:, -1, :] # (B, 1, vocab_size)
+            if temperature > 0:
+                logits = logits / temperature
+            next_tokens = sample_top_p(logits, top_p, generator) # (B, 1)
+            next_tokens = next_tokens.view(-1) # (B,)
+            # We don't want the model to generate only eos token and terminate early
+            eos_reached |= (next_tokens == self.tokenizer.eos_id) & (~input_mask[:, cur_pos])
+            # Update tokens tensor with the newly generated token only if the tokens in the current position are not padding tokens
+            next_tokens = torch.where(input_mask[:, cur_pos], tokens_tensor[:, cur_pos], next_tokens)
+            tokens_tensor[:, cur_pos] = next_tokens
+            # print("Next: ")
+            # print(next_tokens)
+            prev_pos = cur_pos
+            # Stop generate when encounters an EOS token
+            if eos_reached.all():
+                break
+
         output = []
-        # min_prompt_len = min(len(t) for t in tokens)
-        # max_prompt_len = max(len(t) for t in tokens)
-        # assert max_prompt_len <= self.config.block_size
-        # total_len = min(max_gen_len + max_gen_len, self.config.block_size)
-        # batch_size = len(tokens)
-        # for block in self.transformer.h:
-        #     block.attn.cache = KVCache(
-        #         batch_size = batch_size,
-        #         max_seq_length = total_len,
-        #         n_kv_heads = self.config.kv_heads,
-        #         head_size = self.config.n_embd // self.config.n_heads,
-        #         device = block.attn.weight.device
-        #     )
-        for ids in tokens: 
-            x = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
-            while x.size(1) < max_gen_len or x[:, -1] != self.tokenizer.eos_id:
-                logits, _ = self(x)
-                logits = logits[:, -1, :]
-                if temperature > 0:
-                    logits = logits / temperature
-                xcol = sample_top_p(logits, top_p, generator) # Similar to indexing top_k_indices with next_token
-                x = torch.cat((x, xcol), dim=1)
-            output.append(x.tolist())
+        for i, token_ids in enumerate(tokens_tensor.tolist()):
+            # Crop to max_gen_len
+            token_ids = token_ids[:max_gen_len + len(tokens[i])]
+            try:
+                eos_pos = token_ids.index(self.tokenizer.eos_id)
+                token_ids = token_ids[:eos_pos]
+            except:
+                pass
+            output.append(token_ids)
+        
+        # Clear cache
+        for block in self.transformer.h:
+            block.attn.cache = None
+
         return output
     
-    def text_generation(self, texts, max_gen_len, top_p, temperature=1.0, generator=None):
-        tokens = [self.tokenizer.encode(text, bos=True, eos=False) for text in texts]
+    def text_generation(self, text_prompts, max_gen_len=None, top_p=0.9, temperature=1.0, generator=None):
+        if max_gen_len is None:
+            max_gen_len = self.config.block_size - 1 # -1 for the BOS token that is always prepended
+        tokens = [self.tokenizer.encode(text, bos=True, eos=False) for text in text_prompts]
         generated = self.generate(tokens, max_gen_len, top_p, temperature, generator)
-        decoded = []
-        for ids in generated:
-            decoded.append(self.tokenizer.decode(ids))
+        decoded = [self.tokenizer.decode(ids) for ids in generated]
         return decoded
     
     @staticmethod
@@ -344,6 +397,22 @@ else:
         device = "mps"
     print("Device:", device)
 
+# B, T = 32, 2048
+# total_batch_size = 262144
+# grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
+# val_size = 4541735 # Magic number
+# val_accum_steps = val_size // (B * T * ddp_world_size)
+# max_step = 6164 # Approx. 4 epochs
+# warmup_step = 50
+
+B, T = 4, 1024
+total_batch_size = 65536
+grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
+val_size = 4541735 # Magic number
+val_accum_steps = val_size // (B * T * ddp_world_size)
+max_step = 6164 # Approx. 4 epochs
+warmup_step = 50
+
 device_type = 'cuda' if device.startswith('cuda') else 'cpu'
 
 # Using TF32 (if available) for faster matrix multiplication 
@@ -368,17 +437,11 @@ num_params = sum(p.numel() for p in model.parameters())
 if master_process:
     print(f"Number of parameters: {num_params}")
 
-total_batch_size = 262144
-grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
-val_size = 4541735 # Magic number
-val_accum_steps = val_size // (B * T * ddp_world_size)
-max_step = 6164 # Approx. 4 epochs
-
 optimizer = raw_model.configure_optimizer(device_type=device)
 scheduler = CosineLRScheduler(
     max_lr = model_config.max_lr, 
     min_lr = 0.1 * model_config.max_lr, 
-    warmup_step = 50, 
+    warmup_step = warmup_step, 
     max_step = max_step
 )
 
@@ -402,11 +465,11 @@ os.makedirs(log_dir, exist_ok=True)
 log_file = os.path.join(log_dir, 'log.txt')
 log_val_file = os.path.join(log_dir, 'log_val.txt')
     
-for step in range(max_step):
+for step in range(0):
     t0 = time.time()
     loss_accum = 0.0
     # Save checkpoint and eval every 250 steps
-    if (step % 250 == 0 or step == max_step - 1):
+    if ((step != 0 and step % 250 == 0) or step == max_step - 1):
         with torch.no_grad():
             model.eval()
             val_loader.advance(reset=True)
@@ -415,7 +478,7 @@ for step in range(max_step):
                 Xb, Yb = val_loader.next_batch()
                 Xb, Yb = Xb.to(device), Yb.to(device)
                 with torch.autocast(device_type=device_type, dtype=type, enabled=use_amp):
-                    _, loss = model(Xb, Yb)
+                    loss = model.forward_loss(Xb, Yb)
                 loss /= val_accum_steps
                 val_accum += loss.detach()
             if ddp:
@@ -438,7 +501,7 @@ for step in range(max_step):
         if ddp:
             model.require_backward_grad_sync = (micro_step == grad_accum_steps - 1)
         with torch.autocast(device_type=device_type, dtype=type, enabled=use_amp):
-            logits, loss = model(Xb, Yb)
+            loss = model.forward_loss(Xb, Yb)
         loss /= grad_accum_steps # Gradients accumulation
         loss_accum += loss.detach() # No need to compute gradients 
         scaler.scale(loss).backward()
@@ -464,13 +527,19 @@ for step in range(max_step):
         tok_per_sec = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size / dt
         print(f'Step: {step} | Loss: {loss_accum.item():.6f} | LR: {lr:.4e} | Time: {1000 * dt:.2f}ms | Tok/sec: {tok_per_sec:.2f}')
 
+model = PiLlama.load_checkpoint('logs/pillama_6163.pt')
+
 prompts = [
-    "Lily likes cats and dogs. She asked her mom for a dog and her mom said no, so instead she asked"
+    "Once upon a time",
 ]
 rng = torch.Generator(device=device)
-rng.manual_seed(42)
-generated = model.text_generation(prompts, max_gen_len=128, top_p=0.85, temperature=1., generator=rng)
+rng.manual_seed(0)
+t0 = time.time()
+generated = model.text_generation(prompts, max_gen_len=128, top_p=0.9, temperature=1., generator=rng)
+t1 = time.time()
 for text in generated:
-    print(text)
+    print(text) 
+    print(f"Time: {t1 - t0:.2f}s")
+    print(f"Tokens per second: {len(text) / (t1 - t0):.2f}")
 if ddp:
     destroy_process_group()
